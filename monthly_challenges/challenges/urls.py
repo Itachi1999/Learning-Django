2e9e2c58c89079d1c1ac8abc4b13fd3b4881c4a1
Nasipuri <snasipuri@deloitte.com>
2023-11-07 13:14:23 +0530

    Learnt Dynamic Routing

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<month>", view=views.index),
+    path("<str:month>", view=views.monthly_challenge),
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -2,14 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+eat = "Eat 3 meals everyday"
+learn = "Learn new things for 20 mins everyday"
+walk = "Walk for 20 mins every day"
+
+monthly_challenges = {
+    "january": eat,
+    "february": learn,
+    "march": walk,
+    "april": walk,
+    "may": learn,
+    "june": eat,
+    "july": learn,
+    "august": walk,
+    "september": eat,
+    "october": learn,
+    "november": eat,
+    "december": walk,
+}
 
 challenge_text = ""
 
-def index(request, month):
-    if month == "january":
-        challenge_text = "January"
-    elif month == "february":
-        challenge_text = "February"
-    else:
-        return HttpResponseNotFound("Not a month")
+def monthly_challenge(request, month):
+    try: 
+        challenge_text = monthly_challenges[month]
+    except:
+        return HttpResponseNotFound("Not a supported month")
     return HttpResponse(challenge_text)
