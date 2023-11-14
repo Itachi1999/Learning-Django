a0b8d87282057d8b089458117caee1b6ccc8a0aa
Soumya Nasipuri <nasipurisoumya@gmail.com>
2023-11-14 20:31:49 +0530

    Start book project to learn about data models
        - Create project book store
        - Create app book_outlet
        - Create model book

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Book(models.Model):
     # Django models already has a autoincrementing ID field
     title = models.CharField(max_length=50)
     author = models.CharField(max_length=100)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], null=True)
+    is_bestselling = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.title} by {self.author} rated {self.rating}"
 
         
     
