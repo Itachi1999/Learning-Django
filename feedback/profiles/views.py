36045d4912650622a6965aadef6c5ea0f2cd4b88
Soumya Nasipuri <nasipurisoumya@gmail.com>
2023-12-06 11:41:26 +0530

    Added modding

---
+++ feedback/profiles/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+
+
+class CreateProfileView(View):
+    def get(self, request):
+        return render(request, "profiles/create_profile.html")
+
+    def post(self, request):
+        print(request.FILES["image"])
+        return HttpResponseRedirect("/profiles")
+    
+        
